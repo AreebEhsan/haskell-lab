34efe4c8a1b3fc5ba6d204365fc73575360cf15b
Areeb <49764279+AreebEhsan@users.noreply.github.com>
2026-02-28 19:17:28 -0500

    Haskell syntax practice

---
+++ app/input-hello.hs
@@ -0,0 +1,7 @@
+main :: IO()
+main = do 
+
+    putStrLn "Enter your name:"
+    name <- getLine
+    putStrLn("Hello,"++ name)
+    
---
+++ app/sum.hs
@@ -0,0 +1,9 @@
+import Data.List
+import System.IO
+
+sumOfNumbers :: Integer
+sumOfNumbers = sum [0..100]
+
+main :: IO ()
+
+main = print sumOfNumbers
